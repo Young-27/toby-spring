da8a891c2041becd968328c12cee6bd35e15aa9a
SunYoung <80451697+Young-27@users.noreply.github.com>
2021-12-28 19:53:31 +0900

    setting

--- chapter01/src/main/java/springbook/user/dao/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package springbook.user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-// 1.3.2 인터페이스의 도입
-public interface ConnectionMaker {
-	public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
